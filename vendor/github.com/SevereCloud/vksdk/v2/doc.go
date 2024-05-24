c092e52ec9bfacdfaa4ec1759ce5f4cc8dca621f
Wim <wim@42.be>
2024-05-24 23:08:09 +0200

    Update mattermost library (#2152)

---
+++ vendor/github.com/SevereCloud/vksdk/v2/doc.go
@@ -0,0 +1,12 @@
+/*
+Package vksdk implements the basic VK API functions.
+
+Source https://github.com/SevereCloud/vksdk
+*/
+package vksdk
+
+// Module constants.
+const (
+	Version = "2.16.1"
+	API     = "5.131"
+)
